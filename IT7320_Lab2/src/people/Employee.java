104c3ae475724fb7fed7afe04d2b2830af0429ee
wang.yuhan <wang.yuhan@B204-01.soit.local>
2018-07-25 14:08:37 +1200

    Lab2 for 320 Course

---
+++ IT7320_Lab2/src/people/Employee.java
@@ -0,0 +1,15 @@
+package people;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		private String employeeID;
+		private String firstName;
+		private String lastName;
+		private String emaill;
+		
+
+	}
+
+}
